ab4fa48893c2a2ca8d0e5eba276512de7552ad72
Younes <122453521+YougatagaPY@users.noreply.github.com>
2023-11-13 17:08:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("en-savoir-plus").addEventListener("click", function() {
+    window.scrollTo({
+        top: document.getElementById("cours").offsetTop,
+        behavior: 'smooth'
+    });
+});
