95d3f51d3658aae6a28ff03a7895eba3d243908e
Younes <122453521+YougatagaPY@users.noreply.github.com>
2023-11-13 17:08:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("en-savoir-plus").addEventListener("click", function() {
-    window.scrollTo({
-        top: document.getElementById("cours").offsetTop,
-        behavior: 'smooth'
-    });
-});
